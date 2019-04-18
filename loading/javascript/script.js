3013b6d00dc836a01dc388e3168dcee2f14dec25
Mythical <mythicalkeyprime@gmail.com>
2019-04-18 11:58:36 -0400

    Un-hardcode rooftops as map

--- loading/javascript/script.js
+++ loading/javascript/script.js
@@ -1,4 +1,4 @@
 function GameDetails(servername, serverurl, mapname, maxplayers, steamid, gamemode){
-  document.getElementById('changer').innerHTML = "Now Playing on: " + " ttt_rooftops_a12"
+  document.getElementById('changer').innerHTML = "Now Playing on: " + mapname
 
 }
